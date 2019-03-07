e425ff24338e70d100ee7d963a1e343e1b1dd087
Nick Cernis <nick@cern.is>
2019-03-07 11:03:25 +0200

    Fix failing tests
    WP.org returns 5.1 for minor versions, rather than 5.1.0.
    
    So it's safer to check that the response value contains 'x.x' rather
    than expecting it to be five characters long.

--- src/wporg.test.js
+++ src/wporg.test.js
@@ -3,11 +3,13 @@ const wporg = require("./wporg.js");
 // getWPVersion()
 test("WP.org returns a WP version number", () => {
   expect.assertions(1);
-  return expect(wporg.getWPVersion()).resolves.toHaveLength(5);
+  return expect(wporg.getWPVersion()).resolves.toMatch(/^\d+\./);
 });
 
 // getPluginVersion()
 test("WP.org returns a WooCommerce version number", () => {
   expect.assertions(1);
-  return expect(wporg.getPluginVersion("woocommerce")).resolves.toHaveLength(5);
+  return expect(wporg.getPluginVersion("woocommerce")).resolves.toMatch(
+    /^\d+\./
+  );
 });
